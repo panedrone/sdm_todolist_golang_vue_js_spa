7381e08dea846ba43db0c2b42a6349f6755e7e31
panedrone <a.merezkiy@gmail.com>
2024-02-13 02:56:19 +0200

    [+] testing gorm preload

--- gorm/dbal/tasks_dao_ex.go
+++ gorm/dbal/tasks_dao_ex.go
@@ -8,71 +8,52 @@ import (
 
 // Hand coded additions
 
-func (dao *TasksDao) ReadProjectTasks(ctx context.Context, pId int64) (res []*models.TaskLi, err error) {
-
-	// SELECT `tasks`.`t_id`,`tasks`.`t_priority`,`tasks`.`t_date`,`tasks`.`t_subject` FROM `tasks` WHERE p_id = 1 ORDER BY t_date, t_id
-
-	where := &models.Task{
+func (dao *TasksDao) _ReadProjectTasks(ctx context.Context, pId int64) (res []*models.TaskLi, err error) {
+	// "Table" and "direct TaskLi" require "Select":
+	//		err = dao.ds.Session(ctx).Table("tasks"). // --> "SELECT * FROM ..."
+	//		err = dao.ds.Session(ctx). // --> "SELECT * FROM ..."
+	// "Model" is OK with no "Select".
+	model := &models.Task{
 		TaskBase: models.TaskBase{PId: pId},
 	}
-
-	// err = dao.ds.Session(ctx). // "Table" and "Model" are not needed if "TaskLi" has "TableName".
-	// err = dao.ds.Session(ctx).Table("tasks"). // Use "Table" or "Model" if "TaskLi" has no "TableName".
-	err = dao.ds.Session(ctx).Model(where).
-		// Select("t_id", "t_date", "t_subject", "t_priority"). // "Select" is not needed!!!
-		Where(where).
+	err = dao.ds.Session(ctx).Model(model). // --> "SELECT t_id, t_date, t_subject, t_priority FROM ..."
+		// Select("t_id", "t_date", "t_subject", "t_priority").
+		Where(model). // // https://gist.github.com/WangYihang/7d43d70db432ff8f3a0a88425bfca7f2
 		Order("t_date, t_id").Find(&res).Error
-
 	return
 }
 
-func (dao *TasksDao) _ReadProjectTasks2(ctx context.Context, pId int64) (res []*models.TaskLi, err error) {
-
+func (dao *TasksDao) ReadProjectTasks(ctx context.Context, pId int64) (res []*models.TaskLi, err error) {
 	// SELECT * FROM `tasks` WHERE p_id = 2 ORDER BY t_date, t_id
-
-	err = dao.ds.Session(ctx).Model(&models.TaskLi{}).
-		Where("p_id = ?", pId).
-		Order("t_date, t_id").Find(&res).Error
-
+	model := &models.TaskLi{PId: pId}
+	err = dao.ds.Session(ctx).Model(model).
+		// Select("t_id", "t_date", "t_subject", "t_priority").
+		Where(model).Order("t_date, t_id").Find(&res).Error
 	return
 }
 
 func (dao *TasksDao) _ReadProjectTasks3(ctx context.Context, pId int64) (res []*models.TaskLi, err error) {
-
 	// SELECT * FROM `tasks` WHERE p_id = 2 ORDER BY t_date, t_id
-
 	err = dao.ds.Session(ctx).
-		Where("p_id = ?", pId).
-		Order("t_date, t_id").Find(&res).Error
-
+		// Select("t_id", "t_date", "t_subject", "t_priority").
+		Where("p_id = ?", pId).Order("t_date, t_id").Find(&res).Error
 	return
 }
 
-func (dao *TasksDao) _ReadProjectTasks4(ctx context.Context, pId int64) (res []*models.ProjectTaskLi, err error) {
-
+func (dao *TasksDao) _ReadProjectTasks4(ctx context.Context, pId int64) (res []*models.TaskLi, err error) {
 	// SELECT `t_id`,`p_id`,`t_date`,`t_subject`,`t_priority` FROM `tasks` WHERE `tasks`.`p_id` = 5 ORDER BY t_date, t_id
-
-	var project = &models.ProjectWithTasks{
+	var model = &models.ProjectWithTasks{
 		Project: models.Project{PId: pId},
 	}
-
-	err = dao.ds.Session(ctx).Model(project).Preload(models.RefProjectTasks, func(db *gorm.DB) *gorm.DB {
-
-		// Use "Select" because "Preload" default yields " SELECT * FROM ..."
-
-		return db.Select("t_id", "p_id", "t_date", "t_subject", "t_priority").
-			Order("t_date, t_id")
-
-	}).
-		Where(project). // https://gist.github.com/WangYihang/7d43d70db432ff8f3a0a88425bfca7f2
-
-		Take(&project).Error
-
-	if err != nil {
-		return
+	err = dao.ds.Session(ctx).Model(model).Preload(models.RefProjectTasks,
+		func(db *gorm.DB) *gorm.DB {
+			// Use "Select" because "Preload" default issues "SELECT * FROM ..."
+			return db.Select("t_id", "p_id", "t_date", "t_subject", "t_priority").
+				Order("t_date, t_id")
+		}).
+		Where(model).Take(&model).Error
+	if err == nil {
+		res = model.RefTasks
 	}
-
-	res = project.RefTasks
-
 	return
 }
--- gorm/models/task_ex.go
+++ gorm/models/task_ex.go
@@ -12,31 +12,13 @@ func (t *TaskLi) TableName() string {
 	return "tasks"
 }
 
-//type TaskProjectFK struct {
-//	PId int64 `gorm:"column:p_id;not null"`
-//}
-//
-//func (t *TaskProjectFK) TableName() string {
-//	return "projects"
-//}
-
-type ProjectTaskLi struct {
-	TaskLi
-	ProjectId int64 `gorm:"column:p_id;not null"`
-	// Project   TaskProjectFK `gorm:"ForeignKey:ProjectId;references:PId"`
-}
-
 const (
-
-	// "RefTasks" is the name of field "ProjectWithTasks.RefTasks".
-	// It is used for "Preload" test.
-
 	RefProjectTasks = "RefTasks"
 )
 
 type ProjectWithTasks struct {
 	Project
-	RefTasks []*ProjectTaskLi `gorm:"ForeignKey:ProjectId;references:PId"`
+	RefTasks []*TaskLi `gorm:"ForeignKey:PId;references:PId"`
 }
 
 func (t *ProjectWithTasks) TableName() string {
